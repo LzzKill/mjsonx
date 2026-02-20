0054fc31eaacef4528976997a7564f094f68f6cf
LzzKill <76607157+LzzKill@users.noreply.github.com>
2026-02-20 16:14:23 +0800

    重构 mjsonx 模块并引入 JSON_Object 基类
    重构 mjsonx.ixx，将类型定义集中到 mjsonx 模块，新增 JSON_Object_t 类并通过导出别名 JSON_Object 供用户继承。demo1.cpp 采用新的模块导入方式，演示了基于 JSON_Object 的结构体定义，提升了模块封装性和用户自定义 JSON 对象的便利性。

--- module/mjsonx.ixx
+++ module/mjsonx.ixx
@@ -1,3 +1,19 @@
 ﻿module;
 
 export module mjsonx;
+
+export import mjsonx.types;
+
+namespace mjsonx
+{
+  class JSON_Object_t {
+  public:
+  };
+
+
+} // namespace mjsonx
+
+namespace mjsonx
+{
+  export using JSON_Object = JSON_Object_t;
+}
--- test/demo1.cpp
+++ test/demo1.cpp
@@ -1,11 +1,27 @@
 ﻿#include <iostream>
 
-import mjsonx.types;
-import mjsonx.utils;
+import mjsonx;
 
 int main()
 {
-  mjsonx::type::boolean m(true);
+  using namespace mjsonx;
+
+  struct mjson : JSON_Object {
+
+    type::integer age{ 1 };
+    type::string name{ "M" };
+    type::array_same<type::string> body;
+    type::array<type::string, type::array<type::string, type::integer>> other;
+  };
+
+  /*
+   * {
+   *    "age": 1,
+   *    "name": "M",
+   *    "body": ["Fe!", "Lam" ],
+   *    "other" : []
+   * }
+   */
 
   std::cout << m.to_string();
 
